15c71e23e590feb69545722f40b8c5a7d50a978b
Thomas Venries <thomas.venries@gmail.com>
2016-10-26 14:49:01 +0200

    Blessed lux.sh

--- src/lux.sh
+++ src/lux.sh
@@ -1,4 +1,19 @@
 #!/bin/bash -
+#
+# Copyright 2016 Thomas "Ventto" Venriès <thomas.venries@gmail.com>
+#
+# Lux is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Lux is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Lux.  If not, see <http://www.gnu.org/licenses/>.
 
 usage() {
 	echo -e "Usage: lux [OPTION]...
