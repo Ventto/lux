9d68bb148f09210fb0669fe74188cec345900363
Thomas Venries <thomas.venries@gmail.com>
2017-03-18 16:31:10 +0100

    src: Fixed optind place, quoting and POSIX-compliant.

--- src/lux.sh
+++ src/lux.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 # Copyright 2016 Thomas "Ventto" Venriès <thomas.venries@gmail.com>
 #
 # Lux is free software: you can redistribute it and/or modify
@@ -13,66 +13,68 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Lux.  If not, see <http://www.gnu.org/licenses/>.
-set -e
-
 usage() {
-    echo -e "Usage: lux [OPTION]...
-Brightness's option values are positive integers.\n
-For percent mode: add '%' after values. Percent mode can only be used with
-brightness value options.
+    echo 'Usage: lux [OPTION]...
+Brightness option values are positive integers.
+Percent mode, add "%" after values (operation options only).
+
+Without option, it prints controller name and brightness info:
+{controller} {min;value;max}
 
 Information:
-  blank:\tPrints controller's name and brightness info;
-  \t\tpattern: {controller} {min;value;max}
-  -h:\t\tPrints this help and exits
-  -v:\t\tPrints version info and exists
+  -h:  Prints this help and exits
+  -v:  Prints version info and exists
 
 Thresholds (can be used in conjunction):
-  -m:\t\tSet the brightness min (min < max)
-  -M:\t\tSet the brightness max (max > min)
+  -m:  Set the brightness min (min < max)
+  -M:  Set the brightness max (max > min)
 
-Operations:
-  -a:\t\tAdd value
-  -s:\t\tSubtract value
-  -S:\t\tSet the brightness value (set thresholds will be ignored)
+Operations (with percent mode):
+  -a:  Add value
+  -s:  Subtract value
+  -S:  Set the brightness value (set thresholds will be ignored)
 
 Controllers:
-  -c:\t\tSet the controller to use (needs argument).\n\t\tUse any controller name in /sys/class/backlight/ as argument.\n\t\tOtherwise a controller is automatically chosen (default)\n"
+  -c:  Set the controller to use (needs argument).
+       Use any controller name in /sys/class/backlight as argument.
+       Otherwise a controller is automatically chosen (default).
+'
 }
 
 version() {
-    echo -e "Lux 1.0
-Copyright (C) 2016 Thomas \"Ventto\" Venries.\n
+    echo 'Lux 1.1
+Copyright (C) 2016 Thomas "Ventto" Venries.
+
 License GPLv3+: GNU GPL version 3 or later
 <http://gnu.org/licenses/gpl.html>.
 This is free software: you are free to change and redistribute it.
 There is NO WARRANTY; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR
-PURPOSE\n"
+PURPOSE
+'
 }
 
 no_conjunction() {
-    while [ "$#" -ne 0 ] && ! ${1} ; do shift ; done
-    echo "$#"
+    while [ "$#" -ne 0 ]; do $1 && return 1; shift; done; return 0
 }
 
 arg_err() {
-    usage ; exit 2
+    usage; exit 2
 }
 
 is_positive_int() {
-    [[ "${1}" =~ ^[0-9]+$ ]] && echo 1 || echo 0
+    echo "${1}" | grep -E '^[0-9]+$' > /dev/null && return 0; return 1
 }
 
 is_percentage() {
-    echo "${1}" | grep "%$" > /dev/null && echo 1 || echo 0
+    echo "${1}" | grep -E '%$' > /dev/null && return 0; return 1
 }
 
 check_perm() {
-    local _brightness=${1}
-    local udev_rule="/etc/udev/rules.d/99-lux.rules"
+    _brightness="${1}"
+    udev_rule="/etc/udev/rules.d/99-lux.rules"
 
     if [ ! -w "${_brightness}" ] ; then
-        if [ "$(id -u)" != "0" ]; then
+        if [ "$(id -u)" -ne 0 ]; then
             if ! id -nG "${USER}" | grep video > /dev/null ; then
                 echo "Use sudo once to setup group permissions,"
                 echo "to access to controller's brightness from user."
@@ -83,18 +85,18 @@ check_perm() {
         fi
     fi
 
-    if [ "$(id -u)" == "0" ]; then
+    if [ "$(id -u)" -eq 0 ]; then
         if ! cut -d: -f1 /etc/group | grep video > /dev/null ; then
             echo "Group ~video~ does not exist."
             exit 1
         fi
 
-        if ! ls -l "${_brightness}" | grep video > /dev/null ; then
+        if [ "$(ls -l "${_brightness}" | cut -d' ' -f4)" != 'video' ]; then
             if [ ! -f "${udev_rule}" ] ; then
                 echo "${udev_rule}: missing file."
                 exit 1
             fi
-            udevadm control --reload-rules
+            udevadm control -R
             udevadm trigger -c add -s backlight
         fi
 
@@ -108,27 +110,27 @@ check_perm() {
 }
 
 main() {
-    local mFlag=false
-    local MFlag=false
-    local cFlag=false
-    local aFlag=false
-    local sFlag=false
-    local SFlag=false
-    local percent_mode=false
+    mFlag=false
+    MFlag=false
+    cFlag=false
+    aFlag=false
+    sFlag=false
+    SFlag=false
+    percent_mode=false
 
-    while getopts "hvm:M:c:a:s:S:" opt; do
+    while getopts 'hvm:M:c:a:s:S:' opt; do
         case $opt in
-            h)  usage   ; exit ;;
-            v)  version ; exit ;;
-            m)  [ "$(is_positive_int "$OPTARG")" == 0 ] && arg_err
+            h)  usage  ; exit;;
+            v)  version; exit;;
+            m)  ! is_positive_int "$OPTARG" && arg_err
                 mFlag=true
-                mArg=$OPTARG
+                mArg="$OPTARG"
                 ;;
-            M)  [ "$(is_positive_int "$OPTARG")" == 0 ] && arg_err
+            M)  ! is_positive_int "$OPTARG" && arg_err
                 MFlag=true
-                MArg=$OPTARG
+                MArg="$OPTARG"
                 ;;
-            c)  local controller_path="/sys/class/backlight/$OPTARG"
+            c)  controller_path="/sys/class/backlight/$OPTARG"
                 if [ ! -d "${controller_path}" ] ; then
                     echo "${controller_path}: controller not found."
                     exit
@@ -137,42 +139,39 @@ main() {
                 cFlag=true
                 cArg="${controller_path}"
                 ;;
-            a)  [ "$(no_conjunction ${sFlag} ${SFlag})" != 0 ] && arg_err
-                if [ "$(is_percentage "$OPTARG")" == 1 ] ; then
+            a)  ! no_conjunction "${sFlag}" "${SFlag}" && arg_err
+                if is_percentage "$OPTARG"; then
                     percent_mode=true
                     OPTARG=$(echo "$OPTARG" | cut -d % -f 1)
                 fi
-                [ "$(is_positive_int "$OPTARG")" == 0 ] && arg_err
+                ! is_positive_int "$OPTARG" && arg_err
                 aFlag=true
-                valArg=$OPTARG
+                valArg="$OPTARG"
                 ;;
-            s)  [ "$(no_conjunction "${aFlag}" "${SFlag}")" != 0 ] && arg_err
-                if [ "$(is_percentage "$OPTARG")" == 1 ] ; then
+            s)  ! no_conjunction "${aFlag}" "${SFlag}" && arg_err
+                if is_percentage "$OPTARG"; then
                     percent_mode=true
                     OPTARG=$(echo "$OPTARG" | cut -d % -f 1)
                 fi
-                [ "$(is_positive_int "$OPTARG")" == 0 ] && arg_err
+                ! is_positive_int "$OPTARG" && arg_err
                 sFlag=true
-                valArg=$OPTARG
+                valArg="$OPTARG"
                 ;;
-            S)  [ "$(no_conjunction "${aFlag}" "${sFlag}")" != 0 ] && arg_err
-                if [ "$(is_percentage "$OPTARG")" == 1 ] ; then
+            S)  ! no_conjunction "${aFlag}" "${sFlag}" && arg_err
+                if is_percentage "$OPTARG"; then
                     percent_mode=true
                     OPTARG=$(echo "$OPTARG" | cut -d % -f 1)
                 fi
-                [ "$(is_positive_int "$OPTARG")" == 0 ] && arg_err
+                ! is_positive_int "$OPTARG" && arg_err
                 SFlag=true
-                valArg=$OPTARG
+                valArg="$OPTARG"
                 ;;
-            \?) usage ; exit 2 ;;
-            :)  usage ; exit 2 ;;
+            \?) usage; exit 2;;
+            :)  usage; exit 2;;
         esac
     done
 
-    shift "$((OPTIND - 1))"
-
-    local best_controller
-    local best_max=-1
+    best_max=-1
 
     if ${cFlag} ; then
         best_controller=${cArg}
@@ -180,11 +179,11 @@ main() {
     # Try to find the best-max-value controller
     else
         for i in $(echo /sys/class/backlight/*) ; do
-            [ "${i:-1}" == "*" ] && break
+            [ "${i:-1}" = '*' ] && break
             max=$(cat "${i}/max_brightness")
-            if (( "${best_max}" < "${max}" )) ; then
-                best_max=${max}
-                best_controller=${i}
+            if [ "${best_max}" -lt "${max}" ] ; then
+                best_max="${max}"
+                best_controller="${i}"
             fi
         done
         if [ -z "${best_controller}" ]; then
@@ -193,12 +192,12 @@ main() {
         fi
     fi
 
-    local file="${best_controller}/brightness"
+    file="${best_controller}/brightness"
 
     check_perm "${file}"
 
     brightness=$(cat "${file}")
-    best_max=$(( best_max - 1 ))
+    best_max=$((best_max - 1))
 
     # Needs to display the choosen controler
     if [ "$#" -eq 0 ] ; then
@@ -206,35 +205,37 @@ main() {
         exit
     fi
 
+    shift "$((OPTIND - 1))"
+
     if ${SFlag} ; then
         ${percent_mode} && valArg=$(( best_max * valArg / 100 ))
         [ "$valArg" -lt 0 ] && valArg=0
-        [ "$valArg" -gt "$best_max" ] && valArg=${best_max}
-        echo "${valArg}" | /usr/bin/tee "${file}"
+        [ "$valArg" -gt "$best_max" ] && valArg="${best_max}"
+        echo "${valArg}" | tee "${file}"
         exit
     fi
 
-    ${mFlag} && own_min=${mArg} || own_min=0
-    ${MFlag} && own_max=${MArg} || own_max=${best_max}
+    ${mFlag} && own_min="${mArg}" || own_min=0
+    ${MFlag} && own_max="${MArg}" || own_max="${best_max}"
 
     if [ $(( own_max - own_min )) -le 0 ] || \
         [ $(( own_max - own_min )) -gt "${best_max}" ] ; then
         arg_err
     fi
 
-    [ "$brightness" -lt "$own_min" ] && brightness=${own_min}
-    [ "$brightness" -gt "$own_max" ] && brightness=${own_max}
+    [ "$brightness" -lt "$own_min" ] && brightness="${own_min}"
+    [ "$brightness" -gt "$own_max" ] && brightness="${own_max}"
 
     ${percent_mode} && valArg=$(( (own_max - own_min) * valArg / 100 ))
 
     if ${aFlag} ; then
         value=$(( brightness + valArg ))
-        [ "$value" -gt "$own_max" ] && value=${own_max}
-        echo "${value}" | /usr/bin/tee "${file}"
+        [ "$value" -gt "$own_max" ] && value="${own_max}"
+        echo "${value}" | tee "${file}"
     elif ${sFlag} ; then
         value=$(( brightness - valArg ))
-        [ "$value" -lt "$own_min" ] && value=${own_min}
-        echo "${value}" | /usr/bin/tee "${file}"
+        [ "$value" -lt "$own_min" ] && value="${own_min}"
+        echo "${value}" | tee "${file}"
     else
         arg_err
     fi
